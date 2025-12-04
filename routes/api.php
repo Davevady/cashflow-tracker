be2bf09e72c9b5ae1be80c5663435318b9ee5ad6
Davevady <de090802@gmail.com>
2025-12-04 14:23:47 +0700

    refactor: menyederhanakan import controller di api.php dengan menggabungkan TransactionController dan WalletSyncController menjadi satu baris untuk meningkatkan keterbacaan dan maintainability

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,8 @@
 <?php
 
-use App\Http\Controllers\Api\TransactionController;
-use App\Http\Controllers\Api\WalletSyncController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\{TransactionController, WalletSyncController};
 
 Route::get('/user', function (Request $request) {
     return $request->user();
